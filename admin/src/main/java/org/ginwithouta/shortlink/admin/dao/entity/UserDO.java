1d89178d6224e0928278d2da47f7cd1b4853e86c
ginwithouta <ginwithouta@gmail.com>
2023-11-10 15:48:24 +0800

    feature：添加ShardingSphere的加密策略，对敏感字段进行加密存储

--- admin/src/main/java/org/ginwithouta/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/org/ginwithouta/shortlink/admin/dao/entity/UserDO.java
@@ -1,6 +1,9 @@
 package org.ginwithouta.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.time.LocalDateTime;
--- admin/src/test/java/org/nageoffer/shorlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/org/nageoffer/shorlink/admin/test/UserTableShardingTest.java
@@ -10,18 +10,18 @@ public class UserTableShardingTest {
     public static final String SQL = """
             create table t_user_%d
             (
-                id            bigint auto_increment comment 'ID' primary key,
-                username      varchar(250) null comment '用户名',
-                password      varchar(512) null comment '密码',
-                real_name     varchar(256) null comment '真实姓名',
-                phone         varchar(128) null comment '手机号',
-                mail          varchar(512) null comment '邮箱',
-                deletion_time bigint       null comment '注销时间戳',
-                create_time   datetime     null comment '创建时间',
-                update_time   datetime     null comment '修改时间',
-                del_flag      tinyint(1)   null comment '删除标识：0：未删除：1：已删除',
-                constraint idx_unique_username
-                    unique (username) comment 'username数据库唯一性兜底策略'
+                id            bigint auto_increment comment 'ID'
+                primary key,
+                username      varchar(250)         null comment '用户名',
+                password      varchar(512)         null comment '密码',
+                real_name     varchar(256)         null comment '真实姓名',
+                phone         varchar(128)         null comment '手机号',
+                mail          varchar(512)         null comment '邮箱',
+                deletion_time bigint               null comment '注销时间戳',
+                create_time   datetime             null comment '创建时间',
+                update_time   datetime             null comment '修改时间',
+                del_flag      tinyint(1) default 0 null comment '删除标识：0：未删除：1：已删除',
+                constraint idx_unique_username unique (username) comment 'username数据库唯一性兜底策略'
             );""";
 
     public static void main(String[] args) {

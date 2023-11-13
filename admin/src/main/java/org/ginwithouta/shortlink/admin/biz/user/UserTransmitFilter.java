3bb2633b1d25d6e9d925faf958419055bdffea5f
ginwithouta <ginwithouta@gmail.com>
2023-11-13 16:39:21 +0800

    feature：新增用户拦截器URI过滤

--- admin/src/main/java/org/ginwithouta/shortlink/admin/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/ginwithouta/shortlink/admin/biz/user/UserTransmitFilter.java
@@ -1,14 +1,20 @@
 package org.ginwithouta.shortlink.admin.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.ginwithouta.shortlink.admin.common.convention.exception.ClientException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+
+import static org.ginwithouta.shortlink.admin.common.enums.UserErrorCodeEnums.USER_TOKEN_CHECK_FAIL;
 
 /**
  * @package : org.ginwithouta.shortlink.admin.biz.user
@@ -16,6 +22,7 @@ import java.util.List;
  * @Date : 2023 - 11月 - 周六
  * @Desc :
  */
+@Slf4j
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
 
@@ -24,19 +31,35 @@ public class UserTransmitFilter implements Filter {
 
     // TODO: 后续还要进行更新
     private static final List<String> IGNORE_URIS = Lists.newArrayList(
-            "/api/short/link/admin/v1/user/login"
+            "/api/short/link/admin/v1/user/login",
+            // 过滤注册链接
+            // 下面这个链接存在很多个重名但是不同请求方法的链接，因此在下面的内容中需要判断请求链接
+            "/api/short/link/admin/v1/user",
+            "/api/short/link/admin/v1/user/has-username"
+
     );
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
-        // TODO: 将登录接口过滤掉
         if (!IGNORE_URIS.contains(requestURI)) {
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get(CACHE_TOKEN_PREFIX + username, token);
-            if (userInfoJsonStr != null) {
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, IGNORE_URIS.get(1)) && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    throw new ClientException(USER_TOKEN_CHECK_FAIL);
+                }
+                Object userInfoJsonStr;
+                try {
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get(CACHE_TOKEN_PREFIX + username, token);
+                    if (userInfoJsonStr == null) {
+                        throw new ClientException(USER_TOKEN_CHECK_FAIL);
+                    }
+                } catch (Exception e) {
+                    throw new ClientException(USER_TOKEN_CHECK_FAIL);
+                }
                 UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
             }

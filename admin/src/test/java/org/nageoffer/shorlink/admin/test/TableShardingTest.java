16a0577e23a4ed76d7d102db8186df04be989a91
ginwithouta <ginwithouta@gmail.com>
2023-11-14 11:32:23 +0800

    【FEATURE】：短链接分组数据库分库分表

--- admin/src/test/java/org/nageoffer/shorlink/admin/test/TableShardingTest.java
+++ admin/src/test/java/org/nageoffer/shorlink/admin/test/TableShardingTest.java
@@ -6,8 +6,8 @@ package org.nageoffer.shorlink.admin.test;
  * @Date : 2023 - 11月 - 周五
  * @Desc :
  */
-public class UserTableShardingTest {
-    public static final String SQL = """
+public class TableShardingTest {
+    private static final String USER_SQL = """
             create table t_user_%d
             (
                 id            bigint auto_increment comment 'ID'
@@ -24,9 +24,26 @@ public class UserTableShardingTest {
                 constraint idx_unique_username unique (username) comment 'username数据库唯一性兜底策略'
             );""";
 
+    private static final String GROUP_SQL = """            
+            create table t_group_%d
+            (
+                id          bigint auto_increment comment 'ID'
+                    primary key,
+                gid         varchar(32)          null comment '短链接分组标识',
+                name        varchar(64)          null comment '短链接名称',
+                username    varchar(256)         null comment '创建分组的创建人',
+                sort_order  int                  null comment '分组排序',
+                create_time datetime             null comment '创建时间',
+                update_time datetime             null comment '修改时间',
+                del_flag    tinyint(1) default 0 null comment '逻辑删除标识位： 0 未删除； 1 已删除',
+                constraint idx_unique_username_gid
+                    unique (gid, username)
+            );
+            """;
+
     public static void main(String[] args) {
         for (int i = 0; i < 16; ++i) {
-            System.out.printf(SQL, i);
+            System.out.printf(GROUP_SQL, i);
         }
     }
 }

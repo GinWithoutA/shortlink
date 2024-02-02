4e28159ed0023c27d773376c72ba8e8f3b03a3ed
ginwithouta <ginwithouta@gmail.com>
2024-02-02 16:10:04 +0800

    【FEATURE】：新增短链接跳转服务层和服务层接口

---
+++ project/src/main/java/org/ginwithouta/shortlink/project/service/ShortLinkGoToService.java
@@ -0,0 +1,12 @@
+package org.ginwithouta.shortlink.project.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.ginwithouta.shortlink.project.dao.entity.ShortLinkGoToDO;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/2
+ * 短链接跳转服务层接口
+ */
+public interface ShortLinkGoToService extends IService<ShortLinkGoToDO> {
+}
---
+++ project/src/main/java/org/ginwithouta/shortlink/project/service/impl/ShortLinkGoToServiceImpl.java
@@ -0,0 +1,16 @@
+package org.ginwithouta.shortlink.project.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.ginwithouta.shortlink.project.dao.entity.ShortLinkGoToDO;
+import org.ginwithouta.shortlink.project.dao.mapper.ShortLinkGoToMapper;
+import org.ginwithouta.shortlink.project.service.ShortLinkGoToService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/2
+ * 短链接跳转服务层接口实现类
+ */
+@Service
+public class ShortLinkGoToServiceImpl extends ServiceImpl<ShortLinkGoToMapper, ShortLinkGoToDO> implements ShortLinkGoToService {
+}

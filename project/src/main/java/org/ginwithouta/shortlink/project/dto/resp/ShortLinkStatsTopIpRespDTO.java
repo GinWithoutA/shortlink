4a128e6e514233d641cc7604e6f584ae0f19b383
ginwithouta <ginwithouta@gmail.com>
2024-02-04 23:49:23 +0800

    【FEATURE】新增短链接高频 IP 返回DTO

---
+++ project/src/main/java/org/ginwithouta/shortlink/project/dto/resp/ShortLinkStatsTopIpRespDTO.java
@@ -0,0 +1,28 @@
+package org.ginwithouta.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2023/12/3
+ * 短链接高频访问 IP 监控响应参数
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkStatsTopIpRespDTO {
+
+    /**
+     * 访问量
+     */
+    private String ip;
+
+    /**
+     * 访问 IP
+     */
+    private Integer cnt;
+}

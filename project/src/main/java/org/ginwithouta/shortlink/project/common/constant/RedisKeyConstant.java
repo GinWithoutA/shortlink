32609416f6ba4953159eaf3840e58f9ad3b4d2a5
ginwithouta <ginwithouta@gmail.com>
2024-02-14 13:35:51 +0800

    【FEATURE】新增短链接监控消息保存队列 Topic 缓存标识

--- project/src/main/java/org/ginwithouta/shortlink/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/org/ginwithouta/shortlink/project/common/constant/RedisKeyConstant.java
@@ -10,15 +10,20 @@ public class RedisKeyConstant {
     /**
      * 短链接跳转前缀Key
      */
-    public static final String GOTO_SHORT_LINK_KEY = "short-link-goto-%s";
+    public static final String GOTO_SHORT_LINK_KEY = "short-link:goto:%s";
 
     /**
      * 短链接跳转分布式锁前缀Key
      */
-    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link-lock-goto-%s";
+    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link:goto:goto:%s";
 
     /**
      * 短链接跳转是否为空 Key
      */
-    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link-is-null-goto-%s";
+    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link:goto:is-null:%s";
+
+    /**
+     * 短链接监控消息保存队列 Topic 缓存标识
+     */
+    public static final String SHORT_LINK_STATS_STREAM_TOPIC_KEY = "short-link:stats-stream";
 }

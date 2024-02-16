35ec4d52f0cc5880aa095bec07340150ec09700a
ginwithouta <ginwithouta@gmail.com>
2024-02-16 13:56:48 +0800

    【FEATURE】新增 Stream 初始化任务

---
+++ project/src/main/java/org/ginwithouta/shortlink/project/initialize/ShortLinkStatsStreamInitializeTask.java
@@ -0,0 +1,27 @@
+package org.ginwithouta.shortlink.project.initialize;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import static org.ginwithouta.shortlink.project.common.constant.RedisKeyConstant.REDIS_STREAM_SHORT_LINK_STATS_GROUP_KEY;
+import static org.ginwithouta.shortlink.project.common.constant.RedisKeyConstant.REDIS_STREAM_SHORT_LINK_STATS_TOPIC_KEY;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/16
+ * 初始化短链接监控消息队列消费者组
+ */
+@Component
+@RequiredArgsConstructor
+public class ShortLinkStatsStreamInitializeTask implements InitializingBean {
+    private final StringRedisTemplate stringRedisTemplate;
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Boolean hasKey = stringRedisTemplate.hasKey(REDIS_STREAM_SHORT_LINK_STATS_TOPIC_KEY);
+        if (hasKey == null || !hasKey) {
+            stringRedisTemplate.opsForStream().createGroup(REDIS_STREAM_SHORT_LINK_STATS_TOPIC_KEY, REDIS_STREAM_SHORT_LINK_STATS_GROUP_KEY);
+        }
+    }
+}

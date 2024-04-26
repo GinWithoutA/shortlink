972d433d06186eb2de214416aaafd67b3c5324bc
ginwithouta <ginwithouta@gmail.com>
2024-04-26 18:02:20 +0800

    【FEATURE】新增数据库新用户初始化文件

--- sql/initial.sql
+++ sql/initial.sql
@@ -1,3 +1,9 @@
+# 创建数据库，同时创建一个新用户，这个数据库的所有修改都由当前用户完成
+CREATE DATABASE IF NOT EXISTS `shortlink` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE USER 'link'@'%' IDENTIFIED BY '你的密码';
+GRANT ALL PRIVILEGES ON shortlink.* TO 'link'@'%';
+FLUSH PRIVILEGES;
+
 # 可有可无
 INSERT INTO `t_group_15` (`id`, `gid`, `name`, `username`, `sort_order`, `create_time`, `update_time`, `del_flag`)
 VALUES (1752265619253805057, 'tSUBMP', '默认分组', 'admin', 0, '2024-01-31 21:00:00', '2024-01-31 21:00:00', 0);
